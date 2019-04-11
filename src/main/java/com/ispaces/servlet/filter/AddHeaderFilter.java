8c88da69c66fac61149f89998826497b92b377c3
Javapoet <dermot@ispaces.com>
2019-04-11 02:00:35 -0500

    Add AddHeaderFilter.java, ContentTypeFilter.java

---
+++ src/main/java/com/ispaces/servlet/filter/AddHeaderFilter.java
@@ -0,0 +1,65 @@
+package com.ispaces.servlet.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+/**
+ * This filter adds a single header to the response configurable in web.xml using the init params `headerName` and `headerValue`
+ */
+
+public class AddHeaderFilter implements Filter {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    private String headerName = null;
+    private String headerValue = null;
+
+    protected FilterConfig filterConfig;
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        logger.debug("init("+filterConfig+")");
+
+        this.filterConfig = filterConfig;
+
+        String headerNameParam = filterConfig.getInitParameter("headerName");
+        if (headerNameParam != null) this.headerName = headerNameParam;
+
+        String headerValueParam = filterConfig.getInitParameter("headerValue");
+        if (headerValueParam != null) this.headerValue = headerValueParam;
+
+        logger.debug("this.headerName = "+this.headerName);
+        logger.debug("this.headerValue = "+this.headerValue);
+        logger.debug(this.headerName+" = "+this.headerValue);
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
+        logger.debug("doFilter(servletRequest, servletResponse, chain)");
+
+        if(
+            servletResponse instanceof HttpServletResponse
+            && this.headerName != null
+        ) {
+
+            logger.debug("servletResponse.addHeader("+this.headerName+", "+this.headerValue+")");
+            ((HttpServletResponse)servletResponse).addHeader(this.headerName, this.headerValue);
+
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    public void destroy() {
+        this.filterConfig = null;
+    }
+
+}
---
+++ src/main/java/com/ispaces/servlet/filter/ContentTypeFilter.java
@@ -0,0 +1,44 @@
+package com.ispaces.servlet.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletResponse;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletException;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+/**
+ * This filter can be used to add a "Content-Type" header to any response.
+ * The content type can be configured in `web.xml`
+ */
+public class ContentTypeFilter implements Filter {
+
+    private static final Logger logger = LogManager.getLogger();
+    private String contentType = null;
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        logger.debug("doFilter(servletRequest, servletResponse, filterChain)");
+
+        ((javax.servlet.http.HttpServletResponse)servletResponse).setContentType(this.contentType);
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        logger.debug("init("+filterConfig+")");
+
+        String contentTypeParam = filterConfig.getInitParameter("contentType");
+        if (contentTypeParam != null) this.contentType = contentTypeParam;
+
+        logger.debug("this.contentType = "+this.contentType);
+    }
+
+    public void destroy() {
+    }
+
+}
