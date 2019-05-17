2956d058eb24a0befc1523e648c4a603aea45754
Javapoet <dermot@ispaces.com>
2019-05-17 01:02:01 -0500

    Add Constants.java

---
+++ src/main/java/com/ispaces/Constants.java
@@ -0,0 +1,18 @@
+package com.ispaces;
+
+public final class Constants {
+
+	public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+	public static final String FILE_SEPARATOR = System.getProperty("file.separator");
+	public static final String PATH_SEPARATOR = System.getProperty("path.separator");
+
+	public static final String EMPTY_STRING = "";
+	public static final String SPACE = " ";
+	public static final String TAB = "\t";
+	public static final String SINGLE_QUOTE = "'";
+	public static final String PERIOD = ".";
+	public static final String DOUBLE_QUOTE = "\"";
+
+	private Constants() {} // private - prevent this class from being constructed
+
+}
